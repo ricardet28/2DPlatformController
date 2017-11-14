8a6d5c9fe6bddcc99bb65860152208ccd99e1664
mariot11 <mariot3r@gmail.com>
2017-11-14 02:57:12 +0100

    Menu y Plataforma que se encoge
    La plataforma se encoge pero no vuelve a su posición inicial y en realidad va a tocar rehacer el código. El menú lo he ajustado y ahora ya se ajusta al tamaño de pantalla, también he añadido un par de animaciones, pero se puede mejorar un poco

--- Assets/Scripts/platforms/PlatformSizeController.cs
+++ Assets/Scripts/platforms/PlatformSizeController.cs
@@ -2,15 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformSizeController : MonoBehaviour {
+public class PlatformSizeController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float xdis, ydis;
+    private float sizePlatformX;
+    public float tamMinimo;
+    private int numRays;
+    private Vector3 sizePlayer;
+    private Vector3 temp;
+    private Renderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        sizePlayer = GameObject.Find("Player").GetComponent<BoxCollider2D>().bounds.size;
+        sizePlatformX = GetComponent<Transform>().localScale.x;
+        tamMinimo = 0.15f;
+        print(sizePlatformX);
+    }
+
+    void FixedUpdate()
+    {
+        xdis = rend.bounds.size.x;
+        ydis = rend.bounds.size.y;
+
+        Vector2 toplefttip = new Vector2(transform.position.x - xdis / 2, transform.position.y + ydis / 2);
+        numRays = (int)Mathf.Ceil(xdis / sizePlayer.x) + 2; //calcula el numero de rayos de forma dinámica dependiendo del tamaño de la plataforma
+
+        for (int i = 0; i < numRays; i++)
+        {
+            Debug.DrawLine(toplefttip + new Vector2((xdis / (numRays - 1)) * i, 0), toplefttip + new Vector2((xdis / (numRays - 1)) * i, +0.05f), Color.green); //Origen, fin, color
+            RaycastHit2D rayosDown = Physics2D.Raycast(toplefttip + new Vector2((xdis / (numRays - 1)) * i, 0), new Vector2(0, 1), 0.05f); //origen, direccion, longitud
+
+            if (rayosDown)
+            {
+                print("colision");
+                if (rayosDown.collider.tag == "Player")
+                {
+                    if (transform.localScale.x > tamMinimo)
+                    {
+                        temp = transform.localScale;
+                        temp.x -= 0.3f * Time.deltaTime;
+                        transform.localScale = temp;
+                    }
+                }
+
+            }
+
+            /*else {
+
+                print("fuera");
+                if (transform.localScale.x < sizePlatformX) {
+                    temp = transform.localScale;
+                    temp.x += Time.deltaTime;
+                    transform.localScale = temp;
+                }
+            }*/
+        }
+    }
 }
+
